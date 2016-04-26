46838023b7143ddb1e14a6626987866829ab6779
Moritz v. Sivers <moritz.vonsivers@lhep.unibe.ch>
2016-04-26 17:23:33 +0200

    fix bug

--- include/HPGeAnalysis.hh
+++ include/HPGeAnalysis.hh
@@ -13,21 +13,21 @@ public:
 public:
     void CalcEfficiencies();
     
-    void SetNEvents(int NEvents) { fNEvents = NEvents; };
+    void SetNEvents(int NEvents) { fNEvents = NEvents; }
     
-    void AddEnergy(double energy) { fenergy.push_back(energy); };
-    void AddG4BR(double G4BR) { fG4BR.push_back(G4BR); };
-    void AddNuDatBR(double NuDatBR) { fNuDatBR.push_back(NuDatBR); };
-    void AddSigRegion(double SigRegion) { fwidth_sig.push_back(SigRegion); };
-    void AddBkgRegion(double BkgRegion) { fwidth_bkg.push_back(BkgRegion); };
+    void AddEnergy(double energy) { fenergy.push_back(energy); }
+    void AddG4BR(double G4BR) { fG4BR.push_back(G4BR); }
+    void AddNuDatBR(double NuDatBR) { fNuDatBR.push_back(NuDatBR); }
+    void AddSigRegion(double SigRegion) { fwidth_sig.push_back(SigRegion); }
+    void AddBkgRegion(double BkgRegion) { fwidth_bkg.push_back(BkgRegion); }
     
-    G4int GetNLines() { return fnlines; };
-    G4double GetEnergy(G4int i) { return fenergy[i]; };
-    G4double GetEfficiency(G4int i) { return fefficiency[i]; };
-    G4double GetEfficiency_err(G4int i) { return fefficiency_err[i]; };
-    G4double GetEffBR(G4int i) { return feffBR[i]; };
+    int GetNLines() { return fnlines; }
+    double GetEnergy(int i) { return fenergy[i]; }
+    double GetEfficiency(int i) { return fefficiency[i]; }
+    double GetEfficiency_err(int i) { return fefficiency_err[i]; }
+    double GetEffBR(int i) { return feffBR[i]; }
 
-    TH1D* GetHisto() { return fhTotEdep; };
+    TH1D* GetHisto() { return fhTotEdep; }
 	
   
 private:
@@ -42,8 +42,8 @@ private:
     std::vector<double> feffBR;
     
     TH1D* fhTotEdep;
-    G4int fnlines;
-    G4int fNEvents;
+    int fnlines;
+    int fNEvents;
     
 };
 
